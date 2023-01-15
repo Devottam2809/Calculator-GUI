4e02c1e047d0686277924ad4eb4a3bfa820b4e51
its_DC <96367023+Devottam2809@users.noreply.github.com>
2023-01-15 12:17:39 +0530

    Update calcc.py

--- calcc.py
+++ calcc.py
@@ -1,17 +1,18 @@
 from tkinter import*
 
-equation=""
+equation=""                         # this will accept the user response as string and then the expression will be evaluated
+
 def show(value):
     global equation
     equation+=value
-    label_result.config(text=equation)
+    label_result.config(text=equation)      # after final evaluation the text will appear on thr result label.
 
-def clear():
+def clear():                                # this function clears the result label, each time when button C is pressed on the calculator
     global equation
     equation=""
-    label_result.config(text=equation)
+    label_result.config(text=equation)      # it simply replaces the text with a blank text.
 
-def calculate():
+def calculate():                            # this function calculates the expression
     global equation
     result=""
     if equation!="":
@@ -20,16 +21,16 @@ def calculate():
         except:
             result="Error"
             equation=""
-    label_result.config(text=result)
+    label_result.config(text=result)        # display of function on result label
 
-root=Tk()
-root.title("Calculator")
-root.geometry("570x600+100+200")
-root.resizable(0,0)
+root=Tk()                               # create object
+root.title("Calculator")                #title of GUI
+root.geometry("570x600+100+200")        # size of calculator
+root.resizable(0,0)                     # fixed size of calculator
 root.configure(bg="#17161b")
-root.iconbitmap("favicon.ico")
+root.iconbitmap("favicon.ico")          # adding favicon to GUI
 
-label_result=Label(root,width=25,height=2,text="",font=("arial",30),)
+label_result=Label(root,width=25,height=2,text="",font=("arial",30),)       # section where the result will be displayed
 label_result.pack()
 
 Button(root,text="c",width=5,height=1,font=("arial",30,"italic"),bd=1,fg="black",bg="green",command=lambda: clear()).place(x=10,y=100)
@@ -54,4 +55,5 @@ Button(root,text="0",width=11,height=1,font=("arial",30,"italic"),bd=1,fg="black
 
 Button(root,text=".",width=5,height=1,font=("arial",30,"italic"),bd=1,fg="black",bg="white",command=lambda: show(".")).place(x=290,y=500)
 Button(root,text="=",width=5,height=3,font=("arial",30,"italic"),bd=1,fg="black",bg="green",command=lambda: calculate()).place(x=430,y=400)
+
 root.mainloop()
